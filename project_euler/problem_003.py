14d658c6759ba70737b2a6f011a80b7c331ef930
Matt Kracht <mwkracht@gmail.com>
2019-06-19 01:14:01 -0400

    Adding faster solution for problem 003

--- project_euler/problem_003.py
+++ project_euler/problem_003.py
@@ -6,6 +6,7 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 import functools
+import math
 
 
 TEST_NUMBER = 600851475143
@@ -62,8 +63,39 @@ def largest_prime_factor(number, factors_method):
     )
 
 
+def get_prime_factorization(number):
+    """
+    Return list of prime integer factorization of provided number.
+
+    This is a more optimal approach that is borrowed from other solutions. The fundamental theory
+    of arithmetic establishes the basis for describing any integer with a unique set of prime
+    factors. This method computes those prime factors.
+
+    The decomposition works by finding the least prime factor in the given number and then
+    finding the least prime factor of the number divided by the previously found factor and so on.
+    """
+    def smallest_prime_factor(n):
+        return next((i for i in range(2, math.floor(math.sqrt(n))) if n % i == 0), n)
+
+    primes = []
+
+    while True:
+        primes.append(smallest_prime_factor(number))
+        if primes[-1] < number:
+            number = number // primes[-1]
+        else:
+            break
+
+    return primes
+
+
+def largest_prime_factor_fast():
+    return get_prime_factorization(TEST_NUMBER)[-1]
+
+
 solutions = [
     functools.partial(largest_prime_factor, TEST_NUMBER, get_factors),
-    functools.partial(largest_prime_factor, TEST_NUMBER, get_factors_faster)
+    functools.partial(largest_prime_factor, TEST_NUMBER, get_factors_faster),
+    largest_prime_factor_fast
 ]
 solution = solutions[-1]
